ecd888bc20d0fe1597eda69067168b7ebdca2ac7
rjowell <russ.jowell@gmail.com>
2018-12-20 00:40:46 -0500

    committed night

---
+++ CampaignFinanceNew/CreateName.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CampaignFinanceNew
+{
+    public partial class CreateName : ContentPage
+    {
+        public CreateName()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CampaignFinanceNew/OfficeSelection.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CampaignFinanceNew
+{
+    public partial class OfficeSelection : ContentPage
+    {
+        public OfficeSelection()
+        {
+            InitializeComponent();
+        }
+    }
+}
