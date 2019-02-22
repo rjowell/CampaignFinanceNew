1051e5c4d5ab221ce8d3100418b241565f7e25e0
Russ Jowell <russ.jowell@gmail.com>
2019-02-22 16:43:03 -0500

    work on create campaign

--- CampaignFinanceNew/CampaignChooser.xaml.cs
+++ CampaignFinanceNew/CampaignChooser.xaml.cs
@@ -24,7 +24,7 @@ namespace CampaignFinanceNew
             }
             else if(sender.StyleId=="cancel")
             {
-
+                Navigation.PushAsync(new CandidateDashboard());
             }
             else
             {
--- CampaignFinanceNew/CampaignDescription.xaml.cs
+++ CampaignFinanceNew/CampaignDescription.xaml.cs
@@ -10,6 +10,25 @@ namespace CampaignFinanceNew
         public CampaignDescription()
         {
             InitializeComponent();
+
+            if(App.newCampaign.isCrowdfund==false)
+            {
+
+                goalLabel.IsVisible = false;
+                dollarEntry.IsVisible = false;
+                endLabel.IsVisible = false;
+                endDateEntry.IsVisible = false;
+
+            }
+        }
+
+        public void ProcessButton(Button button, EventArgs e)
+        {
+            var sendingParameters = new System.Collections.Specialized.NameValueCollection
+            {
+                {"title",App.newCampaign.campaignName},
+                {"isCrowdfund",App.newCampaign.isCrowdfund.ToString()},
+            };
         }
     }
 }
